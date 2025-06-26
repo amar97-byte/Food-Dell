eb88c90d179b6f3a31bc21a3fa41c02c377cf397
Amarjeet <amarjeetgiri020@gmail.com>
2025-06-27 01:19:11 +0530

    components for frontend side

---
+++ Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,46 @@
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Deleviry</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-526-589-8589</li>
+            <li>contact@tomoto.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr/>
+      <p className="footer-copyright">
+        Copyright 2025 ^ Tomato.com - All Rights Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your Favourite Food Here</h2>
+        <p>Choose from diverse menu featuring a delectable array of dishes crafted with the finest ingrediant and culinary expertise.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
